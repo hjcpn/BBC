4b7538b67b5feff48bdd64ce5d24db2e822860ac
liujiachen <1047382370@qq.com>
2016-07-22 20:19:24 +0800

    change database_url to absolutepath

--- BBS/setting/config.py
+++ BBS/setting/config.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+import os
+project_dir=os.path.abspath(os.path.join(os.path.dirname(__file__),'..'))
+database_url=os.path.join(project_dir,'bbs.db')
 SECRET_KEY = 'development key',
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'sqlite:///bbs.db'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(database_url)
 CELERY_BROKER_URL = 'redis://localhost:6379',
 CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
